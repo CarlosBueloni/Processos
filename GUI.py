80cdddb991da24c0c26c6c48a31e4420b46766dc
CarlosBueloni <carlosbueloni@gmail.com>
2020-03-24 13:55:23 -0300

    Added GUI Calendar and confirm button Adicionados Calendario e Botão de confirmação GUI

--- GUI.py
+++ GUI.py
@@ -1,10 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from datetime import datetime
+import calendar
+from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QPushButton, QMessageBox
+from PyQt5.QtCore import QDate
 
-if __name__ == "__main__":
+
+class CalendarDemo(QWidget):
+    global currentYear, currentMonth
+
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Calendar Demo')
+        self.setGeometry(300, 300, 450, 300)
+        self.initUI()
+
+    def initUI(self):
+        self.calendar = QCalendarWidget(self)
+        self.calendar.move(20, 20)
+        self.calendar.setGridVisible(True)
+        self.button = QPushButton('Confirmar')
+        self.calendar.layout().addWidget(self.button)
+
+
+
+        self.calendar.setMinimumDate(QDate(currentYear-10, currentMonth, 1))
+        self.calendar.setMaximumDate(
+            QDate(currentYear, currentMonth + 1, calendar.monthrange(currentYear, currentMonth)[1]))
+
+        self.calendar.setSelectedDate(QDate(currentYear, currentMonth, 1))
+
+        self.calendar.clicked.connect(self.printDateInfo)
+        self.button.clicked.connect(self.on_button_clicked)
+
+    def printDateInfo(self, qDate):
+        print('{0}/{1}/{2}'.format(qDate.month(), qDate.day(), qDate.year()))
+        print(f'Day Number of the year: {qDate.dayOfYear()}')
+        print(f'Day Number of the week: {qDate.dayOfWeek()}')
+
+    def on_button_clicked(self,qDate):
+        alert = QMessageBox()
+        alert.setText('Baixando processos')
+        alert.exec_()
+
+def main():
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(900, 600)
-    w.setWindowTitle('Procurações')
-    w.show()
+    window = CalendarDemo()
+    window.show()
     sys.exit(app.exec_())
+
+
+main()
